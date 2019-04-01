71004b777736e49b7998c90a4676944cc9d05a1f
elferantonio1005 <49157495+elferantonio1005@users.noreply.github.com>
2019-04-01 16:29:23 +0200

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC01-LAB02
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre="elfer";
+  String apellido="ponce";
+System.out.println("concatenacion:"+nombre+apellido);
+System.out.println("nuevalinea:\n"+nombre+" "+apellido);
+System.out.println("tabulador\t"+nombre+" "+apellido);
+System.out.println("retroceso:\b"+nombre+" "+apellido);
+System.out.println("retorno de carro:\r"+nombre+" "+apellido);
+System.out.println("comilla simple:\'"+nombre+"\'"+apellido);
+System.out.println("comilla doble:\""+nombre+" "+apellido+"\"");
+        
+    }
+    
+}
